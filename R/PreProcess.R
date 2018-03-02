41f337273504480da3ae2308377a9c0c9b0cab9a
zhaleh-safikhani <zhalehsafikhani@gmail.com>
2018-03-02 14:24:41 -0500

    Change row names from EntrezGeneId to Ensembl Ids

--- R/PreProcess.R
+++ R/PreProcess.R
@@ -9,48 +9,48 @@
 ############################# 7) HighCutoff: Cutoff for maximum value of the sensitivity of each cell line for which the cell line is considered as in-sensitive
 
 PreProcess <- function(PSet, Tissue, Drug, MolecularProfile, Measure, HighCutoff, LowCutoff){
-  
+
   TargetPset <- downloadPSet(PSet)
-  ExpProfile <- exprs(summarizeMolecularProfiles(TargetPset,  mDataType = MolecularProfile, verbose=TRUE))
-  rownames(ExpProfile) <- as.numeric(fData(TargetPset@molecularProfiles[[MolecularProfile]])[,"EntrezGeneId"])
+  ExpProfile <- exprs(summarizeMolecularProfiles(TargetPset,  mDataType=MolecularProfile, verbose=TRUE, cell.lines=TargetPset@cell$cellid[which(TargetPset@cell$tissueid %in% Tissue)]))
+  col_with_NA <- unique(which(is.na(ExpProfile), arr.ind=T)[,2])
+  ExpProfile <- ExpProfile[, -col_with_NA]
   ############# lung, haematopoietic_and_lymphoid_tissue, skin, central_nervous_system, large_intestine, breast
-  MatchedCellId <- which(TargetPset@cell$tissueid %in% Tissue)
-  TargetCells <- TargetPset@cell$cellid[MatchedCellId]
+  TargetCells <- colnames(ExpProfile)
   #############
-  TissuVec <- factor(TargetPset@cell$tissueid[MatchedCellId])
+  TissuVec <- factor(which(TargetPset@cell$tissueid %in% Tissue)[-col_with_NA])
   #############
   ObsMat <- c()
   for(MeasureIter in 1:length(Measure)){
-    SensProfile <- summarizeSensitivityProfiles(TargetPset, sensitivity.measure = Measure[MeasureIter])
-    
-    ContVec <- SensProfile[which(tolower(as.character(rownames(SensProfile))) == tolower(Drug)), 
+    SensProfile <- summarizeSensitivityProfiles(TargetPset, sensitivity.measure=Measure[MeasureIter])
+
+    ContVec <- SensProfile[which(tolower(as.character(rownames(SensProfile))) == tolower(Drug)),
                            which(toupper(colnames(SensProfile)) %in%
                                    intersect(toupper(colnames(SensProfile)), TargetCells))]
-    
+
     if(Measure[MeasureIter] == "ic50_recomputed"){
-      ClassVec <- ifelse(ContVec < LowCutoff[MeasureIter], 2, ifelse(ContVec > HighCutoff[MeasureIter], 1,NA))
+      ClassVec <- ifelse(ContVec < LowCutoff[MeasureIter], 2, ifelse(ContVec > HighCutoff[MeasureIter], 1, NA))
     }else{
-      ClassVec <- ifelse(ContVec > HighCutoff[MeasureIter], 2, ifelse(ContVec < LowCutoff[MeasureIter], 1,NA))
+      ClassVec <- ifelse(ContVec > HighCutoff[MeasureIter], 2, ifelse(ContVec < LowCutoff[MeasureIter], 1, NA))
     }
 
     ObsMat <- rbind(ObsMat, ClassVec)
   }
-  
+
   SumVec <- apply(ObsMat, 2, function(X){length(unique(X))})
-  TargetInd <- which(apply(ObsMat, 2, function(X){length(unique(X))}) == 1 & 
+  TargetInd <- which(apply(ObsMat, 2, function(X){length(unique(X))}) == 1 &
                        apply(ObsMat, 2, function(X){length(which(is.na(X)))}) == 0)
   Classes <- unlist(apply(ObsMat, 2, unique)[TargetInd])
   ##############
   FeatureMat <- t(as.matrix(ExpProfile[,TargetInd]))
   TissuVec <- TissuVec[TargetInd]
-  
+
   rownames(FeatureMat) <- NULL
   colnames(FeatureMat) <- rownames(ExpProfile)
-  
+
   FeatureFrame <- data.frame(FeatureMat)
   ###########
   ProcessedList <- list(FeatFrame=FeatureFrame, Observations = Classes, Tissues=TissuVec)
-  
+
   return(ProcessedList)
 }
 
